8f32a696cee34da925a72688ac983e4ef14a70bf
GauGoth-Corp <ggaugoth@gmail.com>
2024-05-29 11:56:07 +0200

    0.11

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+# Installer un module python sur VSCode : taper "py -m pip install [nom du module]" dans le terminal 
+# Pour éditer le fichier audio/graphiques (.pyxres), taper "py -m pyxel edit [PYXEL_RESOURCE_FILE]" dans la console
+# (Remplacer [PYXEL_RESOURCE_FILE] par le nom voulu, peu importe)
+# ATTENTION : cette ligne s'exécute dans le dossier courant (pour naviguer dans les dossiers, utiliser la cmd "cd")
+# Docu officielle de Pyxel : https://github.com/kitao/pyxel/blob/main/docs/README.fr.md#comment-cr%C3%A9er-une-ressource
+# Installer Pyxel en portable en local (très pratique à l'école car on a pas les droits admin) : https://nuit-du-code.forge.apps.education.fr/DOCUMENTATION/05-materiel-logiciels#python-pyxel_1
+
 # Copyright © 2024 GauGoth Corp. All rights reserved - http://gaugoth.corp.free.fr
 import pyxel as px
 import random  
