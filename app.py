41e795619202d1e27f7f6136f9ef265865083d51
Thomas <thomas@reltgen.net>
2024-05-29 14:50:49 +0200

    T'inquiètes du commit v3

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ import random
 px.init(256, 256, title="Test")
 
 # chargement des ressources
-px.load("datas.pyxres")
-
+px.load("3.pyxres")
 
 #####################################################
 ##################### Variables #####################
@@ -26,9 +25,9 @@ px.load("datas.pyxres")
 perso_x = 7
 perso_y = 236
 
-#persoAuthMouv = True
+# persoAuthMouv = True
 
-ennemis_liste = [[61, 43], [52, 93], [59, 12]]
+ennemis_liste = []
 
 # initialisation des tirs
 tirs_liste = []
@@ -37,55 +36,48 @@ tirs_liste = []
 rochersListL = [[5, 210], [20, 210], [60, 210], [95, 210], [95, 228], [95, 243]]
 rocherListB = []
 
+perso_pos = '1'
+
+vies = 4
+
+
 #####################################################
 ##################### FONCTIONS #####################
 #####################################################
 
 
 def deplacement_perso(x, y):
-    global rochersListL, perso_x, perso_y#, persoAuthMouv
+    global rochersListL, perso_x, perso_y, perso_pos  # , persoAuthMouv
 
-    if px.btnp(px.KEY_ESCAPE):
-        px.quit
     if px.btn(px.KEY_RIGHT):
-        for rocher in rochersListL:
-            if rocher[0] <= perso_x+23:
-                perso_x = perso_x-1
-            elif (x < 248):
-                x += 1
+        if (x < 248):
+            x = x + 3
+            perso_pos = '1'
     if px.btn(px.KEY_LEFT):
-        for rocher in rochersListL:
-            if rocher[0]+23 >= perso_x:
-                perso_x = perso_x+1
-            elif (x > 0):
-                x -= 1        
-    if px.btn(px.KEY_UP):
-        for rocher in rochersListL:
-            if rocher[0] <= perso_x+23:
-                perso_y = perso_y+1
-            elif (y > 0):
-                y -= 1  
+        if (x > 0):
+            x = x - 3
+            perso_pos = '2'
     if px.btn(px.KEY_DOWN):
-        for rocher in rochersListL:
-            if rocher[0]+23 >= perso_x:
-                perso_y = perso_y+1
-            elif (y < 238):
-                y += 1          
-    return x, y
+        if (y < 248):
+            y = y + 3
+    if px.btn(px.KEY_UP):
+        if (y > 0):
+            y = y - 3
+    return x, y, perso_pos
 
 
 def ennemis_deplacement(ennemis_liste, x_du_perso, y_du_perso):
     for ennemi in ennemis_liste:
         if x_du_perso > ennemi[0]:
-            ennemi[0] += 1
+            ennemi[0] += 0.5
             ennemi[2] = '2'
         if x_du_perso < ennemi[0]:
-            ennemi[0] -= 1
+            ennemi[0] -= 0.5
             ennemi[2] = '1'
         if y_du_perso > ennemi[1]:
-            ennemi[1] += 1
+            ennemi[1] += 0.5
         if y_du_perso < ennemi[1]:
-            ennemi[1] -= 1
+            ennemi[1] -= 0.5
     return ennemis_liste
 
 
@@ -98,7 +90,7 @@ def ennemis_creation(ennemis_liste, x_du_perso):
     if x_du_perso < x_ennemi:
         position = '1'
     # un ennemi par seconde
-    if (px.frame_count % 30 == 0):
+    if (px.frame_count % 60 == 0):
         ennemis_liste.append([x_ennemi, y_ennemi, position])
     return ennemis_liste
 
@@ -118,6 +110,7 @@ def tirs_deplacement(tirs_liste):
 
     return tirs_liste
 
+
 """def rochersL_colisions(x, y):
     global rochersListL, perso_x, perso_y, persoAuthMouv
 
@@ -128,8 +121,39 @@ def tirs_deplacement(tirs_liste):
             persoAuthMouv = False"""
 
 
-#def rochersL_creation(x, y):
+def tirs_creation(x, y, tirs_liste):
+    if px.btnr(px.MOUSE_BUTTON_LEFT) or px.btnr(px.KEY_SPACE):
+        tirs_liste.append([x + 16, y + 2])
+    return tirs_liste
 
+
+def tirs_deplacement(tirs_liste):
+    for tir in tirs_liste:
+        tir[0] += 1
+        if tir[0] > 270:
+            tirs_liste.remove(tir)
+            print("supprimé")
+
+    return tirs_liste
+
+
+def enemi_delet(vies):
+    for ennemi in ennemis_liste:
+        if ennemi[0] <= perso_x + 8 and ennemi[1] <= perso_y + 8 and ennemi[0] + 8 >= perso_x and ennemi[
+            1] + 8 >= perso_y:
+            ennemis_liste.remove(ennemi)
+            vies -= 1
+    return vies
+
+
+def ennemis_suppression():
+    for ennemi in ennemis_liste:
+        for tir in tirs_liste:
+            if ennemi[0] <= tir[0] + 1 and ennemi[0] + 8 >= tir[0] and ennemi[1] + 8 >= tir[1]:
+                ennemis_liste.remove(ennemi)
+                tirs_liste.remove(tir)
+
+            # def rochersL_creation(x, y):
 
 
 #####################################################
@@ -138,8 +162,8 @@ def tirs_deplacement(tirs_liste):
 
 #################### VOID UPDATE ####################
 def update():
-    global perso_x, perso_y, ennemis_liste, tirs_liste
-    perso_x, perso_y = deplacement_perso(perso_x, perso_y)
+    global perso_x, perso_y, ennemis_liste, tirs_liste, vies
+    perso_x, perso_y, perso_pos = deplacement_perso(perso_x, perso_y)
     ennemis_liste = ennemis_deplacement(ennemis_liste, perso_x, perso_y)
     ennemis_liste = ennemis_creation(ennemis_liste, perso_x)
 
@@ -149,10 +173,14 @@ def update():
     # mise a jour des positions des tirs
     tirs_liste = tirs_deplacement(tirs_liste)
 
+    ennemis_suppression()
+    vies = enemi_delet(vies)
+
 
 ##################### VOID DRAW #####################
 
 def draw():
+    global perso_pos, perso_x, perso_y, vies
     # Fond rouge
     px.cls(4)
 
@@ -162,26 +190,35 @@ def draw():
     # Copie la région de taille (w, h) de (u, v) de la banque d’image img(0-2) à (x, y).
     # Si une valeur négative est mise pour w(ou h), la copie sera inversée horizontalement
     # (ou verticalement). Si colkey est spécifiée, elle sera traitée comme une couleur transparente.
+    if perso_pos == '1':
+        px.blt(perso_x, perso_y, 0, 0, 8, 16, 15, 5)
+    elif perso_pos == '2':
+        px.blt(perso_x, perso_y, 0, 0, 8, -16, 15, 5)
 
-    px.blt(perso_x, perso_y, 0, 0, 8, 16, 15, 5)
-
-    #Draw le décor (rochers)
-    #Rochers simples
+    # Draw le décor (rochers)
+    # Rochers simples
     for rocherL in rochersListL:
         px.blt(rocherL[0], rocherL[1], 0, 176, 128, 16, 15, 5)
 
-    #Rochers 3x16
-    px.blt(75, 210, 0, 224, 128, 3*16, 16, 5)
+    # Rochers 3x16
+    px.blt(75, 210, 0, 224, 128, 3 * 16, 16, 5)
+
+    if vies > 0:
+        px.text(5, 5, 'VIES:' + str(vies), 7)
+
+        # tirs
+        for tir in tirs_liste:
+            px.blt(tir[0], tir[1], 0, 48, 8, 7, 7, 5)
+
+        for ennemi in ennemis_liste:
+            if ennemi[2] == '2':
+                px.blt(ennemi[0], ennemi[1], 0, 0, 120, 16, 16, 5)
+            if ennemi[2] == '1':
+                px.blt(ennemi[0], ennemi[1], 0, 0, 120, -16, 16, 5)
+    else:
+        px.text(100, 128, 'GAME OVER', 7)
 
     # tirs
-    for tir in tirs_liste:
-        px.blt(tir[0], tir[1], 0, 48, 8, 7, 7, 5)
-
-    for ennemi in ennemis_liste:
-        if ennemi[2] == '2':
-            px.blt(ennemi[0], ennemi[1], 0, 0, 120, -16, 16, 5)
-        if ennemi[2] == '1':
-            px.blt(ennemi[0], ennemi[1], 0, 0, 120, 16, 16, 5)
 
 
 ##################### EXECUTION DU CODE #####################
